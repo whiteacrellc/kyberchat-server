8d86db1d148c74f23b226f80b7801ff14b91160c
White Acre Software <tom@tomw.net>
2026-03-02 22:05:17 -0600

    Delete cloudrun/deploy.sh
    yeah

--- cloudrun/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gcloud run deploy quantchat-server \
-   --source . \
-   --region us-central1 \
-   --add-cloudsql-instances quantchat-server:us-central1:quantchat \
-   --allow-unauthenticated \
-   --set-env-vars "DB_USER=k8s,DB_PASS=machogrande,DB_NAME=e2e_chat_service,DB_HOST=/cloudsql/quantchat-server:us-central1:quantchat,JWT_SECRET=machogrande,REDIS_URL=redis://10.167.56.211:6379"
